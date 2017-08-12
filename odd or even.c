67c51f7388692962dc6febb3082321885f3506a2
Mathanmass <30368912+Mathanmass@users.noreply.github.com>
2017-08-12 19:31:27 +0530

    Update odd or even.c

--- odd or even.c
+++ odd or even.c
@@ -2,10 +2,10 @@
 int main()
 {
 int a;
-printf("\n enter the value of n");
-scanf("%d",&n);
-if (n%2==0)
+printf("\n enter the value of a");
+scanf("%d",&a);
+if (a%2==0)
 printf("\nthe number is even");
 else
-printf("the number is odd);
+printf("\nthe number is odd);
 }
