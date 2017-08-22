e23a1a5ed942259e75189c69fe4e1b9b606aee20
Mathanmass <30368912+Mathanmass@users.noreply.github.com>
2017-08-22 16:15:39 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,n,s;
+printf("Enter the integer:");
+scanf("%d",&n);
+for(i=1;i<=n:i++)
+{
+s=s+i;
+}
+printf("sum of first %d natural numbers:%d\n",n,s);
+}
