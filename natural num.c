c8eba0ead45072e367aa573b26ae0d6ab68b347b
Mathanmass <30368912+Mathanmass@users.noreply.github.com>
2017-08-22 16:09:11 +0530

    Update natural num.c

--- natural num.c
+++ natural num.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-    int i, num, sum = 0;
+ int i, num, sum = 0;
     printf("Enter an integer number \n");
     scanf ("%d", &num);
     for (i = 1; i <= num; i++)
